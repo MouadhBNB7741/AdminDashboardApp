5fc6361606d917a620c05513141cceee37375940
MouadhBNB7741 <mbenboudh@gmail.com>
2025-06-30 20:26:27 +0100

    added full api list and prepared the server + better the setup

--- backend/controller/User.js
+++ backend/controller/User.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import client from "../services/prisma";
 
 const userRouter = Router();
+
+userRouter.get("/", async (req, res) => {});
 
 export default userRouter;
